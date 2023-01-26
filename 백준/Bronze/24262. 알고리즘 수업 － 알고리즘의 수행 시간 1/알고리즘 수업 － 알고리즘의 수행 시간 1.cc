4b48ff22ba0d38ab0af5cf2a1dfc25a8826d4bd1
DaeHee Han <63997481+hdh4952@users.noreply.github.com>
2023-01-26 21:12:38 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define REP(i, a, b) for(int i=a ; i<=b ; i++)
+using namespace std;
+typedef long long int ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n; cin >> n;
+    cout << "1\n0";
+}
